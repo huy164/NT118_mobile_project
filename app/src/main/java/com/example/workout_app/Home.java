e3c5313c7d5fc709cb214875974a1e64c3b3ed9a
huy164 <vqh.official@gmail.com>
2022-05-13 00:33:37 +0700

    fix bottom naviagation

--- app/src/main/java/com/example/workout_app/Home.java
+++ app/src/main/java/com/example/workout_app/Home.java
@@ -1,8 +1,14 @@
 package com.example.workout_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +16,9 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigatin_view);
+        NavController navController = Navigation.findNavController(this,  R.id.nav_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+//        AppBarConfiguration appBarConfiguration=AppBarConfiguration(setOf(R.id.homeFragment,R.id.profileFragent,R.id.settingFragment));
     }
 }
--- app/src/main/java/com/example/workout_app/Views/fragnents/SettingFragment.java
+++ app/src/main/java/com/example/workout_app/Views/fragnents/SettingFragment.java
@@ -1,4 +1,4 @@
-package com.example.workout_app;
+package com.example.workout_app.Views.fragnents;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.workout_app.R;
 
 /**
  * A simple {@link Fragment} subclass.
