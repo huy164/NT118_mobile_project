bd0f6ee3b784f305b45d05201508f9e8a32ad064
huy164 <vqh.official@gmail.com>
2022-06-14 20:54:44 +0700

    add activity to compute heart rate

---
+++ app/src/main/java/com/example/workout_app/Views/CalculateHeartRate.java
@@ -0,0 +1,16 @@
+package com.example.workout_app.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.workout_app.R;
+
+public class CalculateHeartRate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_heart_rate);
+    }
+}
--- app/src/main/java/com/example/workout_app/Views/Fragnents/ProfileFragment.java
+++ app/src/main/java/com/example/workout_app/Views/Fragnents/ProfileFragment.java
@@ -1,27 +1,32 @@
 package com.example.workout_app.Views.Fragnents;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.workout_app.R;
+import com.example.workout_app.Views.CalculateHeartRate;
+import com.example.workout_app.Views.start_up_screens.UserRegistration;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends  Fragment  {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private Button btnCalHeartRate;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -61,6 +66,16 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        btnCalHeartRate = (Button)rootView.findViewById(R.id.btnCalHeartRate);
+        btnCalHeartRate.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                        startActivity(new Intent(v.getContext(), CalculateHeartRate.class));
+            }
+        });
+        return rootView;
     }
 }
